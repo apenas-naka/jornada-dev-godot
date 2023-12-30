fe46ac112f184facbfffbcb6bb4a65000d3b0114
Naka <92233949+apenas-naka@users.noreply.github.com>
2023-12-29 21:57:28 -0300

    fix and upgrades
    spawn system improvement;

--- scripts/burguer.gd
+++ scripts/burguer.gd
@@ -1,17 +1,24 @@
 extends Area2D
 
 func _ready():
+	var vtimer = $visibilityTimer;
+	vtimer.start(1.5);
 	hide();
-	print("Criado - Invisivel");
+	#print("hide()");
 
 func _on_body_entered(body):	#COLISÃO COM O ITEM
 	if body.is_in_group("walls"):	#	SE ESTIVER NO GRUPO 'PAREDE'
 		print("SPAWNOU NA PAREDE");
 		queue_free();
 		print("REMOVIDO");
-	elif body.is_in_group("player"):
+	if body.is_in_group("player"):
 		print("Coletado pelo jogador!");
 		queue_free();
-	elif body.is_in_group("collectables"):
+	if body.is_in_group("collectables"):
 		print("Dupla colisão");
 		queue_free();
+
+
+func _on_visibility_timer_timeout():
+	show();
+	#print("VisibilityTimer: ON")
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,26 +1,17 @@
 extends Node2D
 
 const myCollectable = preload("res://scenes/burguer.tscn");
+#const limitsOfCollectables = 5;
 
 func _on_spawn_collectables_timeout():	# QUANDO O TIMER spawnCol. acabar:
-	var newObject = myCollectable.instantiate();
-	newObject.position = Vector2(randf_range(0, 2000), randf_range(0, 2000));
-	print(newObject.position);
-	newObject.add_to_group("collectables");
-	add_child(newObject);
-	#newObject.hide();
-	#$visibleCollectables.start();
-	#print("TIMER VISIBLE INIT");
-	#var teste := $visibleCollectables;
-	#teste.wait_time = 1;
-	#teste.start()
-	#teste.timeout.connect()
-	#var teste = $burguer/checkColl;
-	#print(teste);
-	#
+	var rng = RandomNumberGenerator.new();
 
+	var pos_x = rng.randf_range(400, 700);
+	var pos_y = rng.randf_range(80, 800);
 
-func _on_visible_collectables_timeout():
-	var collect = $burguer;
-	collect.show();
-	print("timer show()")
+	var spawned_Obj = myCollectable.instantiate();
+	spawned_Obj.global_position.x = pos_x;
+	spawned_Obj.global_position.y = pos_y;
+	#print("POS: " + str(spawned_Obj.position));
+	spawned_Obj.add_to_group("collectables");
+	add_child(spawned_Obj);
